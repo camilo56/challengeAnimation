f1c9d0defc2793f699d136b0e62074507650944d
camilo.colorado <camilo.colorado@pragma.com.co>
2017-10-04 16:47:57 -0500

    ajuste finales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, stagger, query } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -7,58 +7,73 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('camiloAnimation', [
-        state('normal', style({
-          'border-radius': '0px',
-          'transform': 'rotateX(0deg)',
+      state('normal', style({
+        'opacity': '1',
         })),
-        state('radius', style({
-          'border-radius': '50%',
-        })),
-        state('rote', style({
-          'transform': 'rotateX(0deg)',
-        })),
-        transition('normal => radius', animate('300ms ease-in')),
-        transition('normal => rote', animate('1500ms',  keyframes([
-          style({transform: 'rotateX(0deg)'}),
-          style({transform: 'rotateX(180deg)'}),
-          style({transform: 'rotateX(0deg)'})
-        ]))),
-        transition('normal => crazy', animate('2000ms',  keyframes([
-          style({transform: 'rotate(0deg)'}),
-          style({transform: 'rotate(180deg)', height:  '60vh'}),
-          style({transform: 'rotate(360deg)'}),
-          style({transform: 'rotate(540deg)', height:  '80vh'}),
-          style({transform: 'rotate(720deg)'}),
-          style({transform: 'rotate(900deg)', height:  '90vh'}),
-          style({transform: 'rotate(1080deg)'}),
-          style({transform: 'rotate(1280deg)', height:  '100vh'}),
-          style({transform: 'rotate(1440deg)'}),
-        ]))),
-        transition('normal => hinge', animate('4000ms ease-in',  keyframes([
-          style({
-            'transform-origin': 'top left',
-            'animation-timing-function': 'ease-in-out'}),
-          style({'transform': 'rotate(80deg)',
-                  'transform-origin': 'top left',
-                  'animation-timing-function': 'ease-in-out'}),
-          style({'transform': 'rotate(60deg)',
-                  'transform-origin': 'top left',
-                  'animation-timing-function': 'ease-in-out',
-                  'opacity': 1}),
-          style({'transform': 'translate3d(0,700px,0)', 'opacity': 0}),
-          style({'transform-origin': 'top left',
-                  'animation-timing-function': 'ease-in-out'})
-        ]))),
-        transition('* => normal', animate('50ms ease-in')),
+      state('radius', style({
+        'border-radius': '50%',
+      })),
+      state('rote', style({
+        'transform': 'rotateX(0deg)',
+      })),
+      transition('normal => radius', animate('1s ease-in')),
+      transition('normal => rote', animate('1500ms',  keyframes([
+        style({transform: 'rotateX(0deg)'}),
+        style({transform: 'rotateX(180deg)'}),
+        style({transform: 'rotateX(0deg)'})
+      ]))),
+      transition('normal => crazy', animate('2000ms',  keyframes([
+        style({transform: 'rotate(0deg)'}),
+        style({transform: 'rotate(180deg)', height:  '60vh'}),
+        style({transform: 'rotate(360deg)'}),
+        style({transform: 'rotate(540deg)', height:  '80vh'}),
+        style({transform: 'rotate(720deg)'}),
+        style({transform: 'rotate(900deg)', height:  '90vh'}),
+        style({transform: 'rotate(1080deg)'}),
+        style({transform: 'rotate(1280deg)', height:  '100vh'}),
+        style({transform: 'rotate(1440deg)'}),
+      ]))),
+      transition('normal => hinge', animate('2000ms ease-in',  keyframes([
+        style({
+          'transform-origin': 'top left',
+          'animation-timing-function': 'ease-in-out'}),
+        style({'transform': 'rotate(80deg)',
+                'transform-origin': 'top left',
+                'animation-timing-function': 'ease-in-out'}),
+        style({'transform': 'rotate(60deg)',
+                'transform-origin': 'top left',
+                'animation-timing-function': 'ease-in-out',
+                'opacity': 1}),
+        style({'transform': 'translate3d(0,700px,0)', 'opacity': 0}),
+        style({'transform-origin': 'top left',
+                'animation-timing-function': 'ease-in-out'})
+      ]))),
+      transition('normal => bye', animate('1s ease-in', style({ opacity: 0 }))),
+      transition('* => normal', animate('50ms ease-in')),
+    ]),
+    trigger('opacity', [
+      transition('* => opOne',
+        [
+          query('.opacity',
+            stagger('800ms', animate(900, style({ opacity: 0 })))
+          ),
+        ]
+      ),
     ]),
   ]
 })
 
 export class AppComponent {
   state = 'normal';
+  stateOne: string;
 
   normalState() {
     this.state = 'normal';
-    console.log('normalState: ', this.state);
   }
+
+  doOpacity() {
+    console.log('doOpacity');
+    this.stateOne = 'opOne';
+  }
+
 }
